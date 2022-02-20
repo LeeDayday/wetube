e76e5a414d4a9e7e977df18ff55fa46eea9cc7a2
LeeDayday <95182422+LeeDayday@users.noreply.github.com>
2022-02-21 00:08:01 +0900

    day3

--- src/server.js
+++ src/server.js
@@ -4,16 +4,33 @@ const PORT = 4000;
 
 const app = express();
 
+const loggerMiddleware = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("Allowed, You may Continue");
+    next();
+};
+
 const handleHome = (req, res) => {
-    return res.send("Welcome to my Home page");
-}
+    return res.send("I love middleware");
+};
 
-const handleLogin = (req, res) => {
-    return res.send("Login here");
-}
+const handleProtected = (req, res) => {
+    console.log("handleProtected");
+    return res.send("Welcome to the private lounge");
+};
 
+app.use(loggerMiddleware);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login",handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () => 
 console.log(`Server listening on port http://localhost:${PORT}🎯`);
