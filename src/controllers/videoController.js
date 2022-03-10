ea89b652becbb7830d2fe69b51dac621a9be075d
LeeDayday <95182422+LeeDayday@users.noreply.github.com>
2022-03-11 00:07:14 +0900

    day6

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trendings = (req, res) => res.send("Trending Videos");
-export const see = (req, res) => res.send(`Watch Video ${req.params.id}`);
-export const edit = (req,res) => res.send("Edit");
+export const trendings = (req, res) => res.render("home", {pageTitle: "Home"});
+export const see = (req, res) => res.render("watch");
+export const edit = (req,res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
